02e1e7284069ecdf776f041f6657f334921062d9
Sofia Miranda <sofia8miranda@hotmail.com>
2025-07-29 00:09:51 +0000

    Feature complete: Dictionary check of attempt

--- src/longest_word/game.py
+++ src/longest_word/game.py
@@ -1,5 +1,6 @@
 import string
 import random
+import requests
 
 class Game:
     def __init__(self) -> list:
@@ -11,7 +12,7 @@ class Game:
 
     def is_valid(self, word: str) -> bool:
         """Return True if and only if the word is valid, given the Game's grid"""
-        if not word:
+        '''if not word:
             return False
         letters = self.grid.copy() # Consume letters from the grid
         for letter in word:
@@ -19,4 +20,11 @@ class Game:
                 letters.remove(letter)
             else:
                 return False
-        return True
+        return True'''
+        return self.__check_dictionary(word)
+
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://dictionary.lewagon.com/{word}")
+        json_response = response.json()
+        return json_response['found']
